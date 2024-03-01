89ba0491633f2d6dce9b31d3cd1d9350fc390b1d
Athira <athiranass@gmail.com>
2024-03-01 10:33:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+let fighter1=fighters[Math.floor(Math.random()*fighters.length)];
+let fighter2=fighters[Math.floor(Math.random()*fighters.length)];
+
+fightButton.addEventListener("click",function()
+{
+
+    var fighter1=fighters[Math.floor(Math.random()*fighters.length)];
+    var fighter2=fighters[Math.floor(Math.random()*fighters.length)];
+
+    console.log(fighter1)
+    console.log(fighter2)
+
+    document.getElementById("one").innerHTML=fighter1
+    document.getElementById("two").innerHTML=fighter2
+    
+
+})
